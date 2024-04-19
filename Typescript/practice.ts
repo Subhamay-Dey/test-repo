41b06b074e0c42201f5ed4cb93f87720bda94f3a
Subhamay-Dey <ncdey1966@gmail.com>
2024-04-19 23:50:10 +0530

    type safety in functions and while node annotatiion.

---
+++ Typescript/practice.ts
@@ -0,0 +1,13 @@
+function addTwo(num: number) {
+    return num + 2
+}
+
+function getUpper(val: string){
+    return val.toUpperCase()
+}
+
+addTwo(4)
+
+getUpper("practicing typescript")
+
+export{}
